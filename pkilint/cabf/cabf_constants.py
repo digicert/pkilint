4c237733c0e9a444fecfd64a18161a9d23d28787
Corey Bonnell <corey.bonnell@digicert.com>
2023-06-08 10:17:05 -0400

    Disallow Registration References in GOV and INT orgIDs (#12)

--- pkilint/cabf/cabf_constants.py
+++ pkilint/cabf/cabf_constants.py
@@ -12,10 +12,11 @@ class RegistrationSchemeCountryIdentifierType(enum.IntEnum):
 class RegistrationSchemeNamingConvention(typing.NamedTuple):
     country_identifier_type: RegistrationSchemeCountryIdentifierType
     allow_state_province: bool
+    require_registration_reference: bool
 
 
 REGISTRATION_SCHEMES = {
-    'NTR': RegistrationSchemeNamingConvention(RegistrationSchemeCountryIdentifierType.ISO3166, True),
-    'VAT': RegistrationSchemeNamingConvention(RegistrationSchemeCountryIdentifierType.ISO3166, False),
-    'PSD': RegistrationSchemeNamingConvention(RegistrationSchemeCountryIdentifierType.ISO3166, False),
+    'NTR': RegistrationSchemeNamingConvention(RegistrationSchemeCountryIdentifierType.ISO3166, True, True),
+    'VAT': RegistrationSchemeNamingConvention(RegistrationSchemeCountryIdentifierType.ISO3166, False, True),
+    'PSD': RegistrationSchemeNamingConvention(RegistrationSchemeCountryIdentifierType.ISO3166, False, True),
 }
