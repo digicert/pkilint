afe68f18b601c26ce55831a7908c93690cc6dd5e
Corey Bonnell <corey.bonnell@digicert.com>
2023-10-25 15:07:58 -0400

    Clean up Docker entrypoint script

--- docker/entrypoint.py
+++ docker/entrypoint.py
@@ -1,30 +1,8 @@
 #!/usr/bin/env python3
 
-import functools
 import sys
 import os
-
-from pkilint.bin import (
-    lint_cabf_serverauth_cert,
-    lint_cabf_smime_cert,
-    lint_crl,
-    lint_ocsp_response,
-    lint_pkix_cert,
-    lint_pkix_signer_signee_cert_chain,
-)
-
-_ENTRYPOINTS = {
-    'lint_cabf_serverauth_cert': lint_cabf_serverauth_cert.main,
-    'lint_cabf_smime_cert': lint_cabf_smime_cert.main,
-    'lint_crl': lint_crl.main,
-    'lint_ocsp_response': lint_ocsp_response.main,
-    'lint_pkix_cert': lint_pkix_cert.main,
-    'lint_pkix_signer_signee_cert_chain': lint_pkix_signer_signee_cert_chain.main,
-}
-
-
-def _run(cmd, args):
-    os.execvp(cmd, [cmd] + args)
+import importlib
 
 
 def main():
@@ -36,9 +14,14 @@ def main():
     cmd = sys.argv[1]
     args = sys.argv[2:]
 
-    entrypoint_func = _ENTRYPOINTS.get(cmd, functools.partial(_run, cmd))
+    try:
+        module = importlib.import_module(f'pkilint.bin.{cmd}')
 
-    return entrypoint_func(args)
+        main_func = getattr(module, 'main')
+    except (ImportError, AttributeError):
+        os.execvp(cmd, [cmd] + args)
+
+    return main_func(args)
 
 
 if __name__ == '__main__':
