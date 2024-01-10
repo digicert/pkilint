24aef96f56a3a832243fafbcf17f2105dd11a6d1
Corey Bonnell <corey.bonnell@digicert.com>
2024-01-10 14:07:26 -0500

    Add support for pre-certs and final certs

--- pkilint/etsi/__init__.py
+++ pkilint/etsi/__init__.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from pyasn1_alt_modules import rfc5280
+from pyasn1_alt_modules import rfc5280, rfc6962
 
 from pkilint import validation, finding_filter
 from pkilint.cabf import serverauth
@@ -18,22 +18,36 @@ def determine_certificate_type(cert: certificate.RFC5280Certificate) -> Certific
     policy_oids = cert.policy_oids
 
     is_qualified = en_319_412_5_asn1.id_etsi_qcs_QcCompliance in qualified_statement_ids
+    is_precert = cert.get_extension_by_oid(rfc6962.id_ce_criticalPoison) is not None
 
     if serverauth_constants.ID_POLICY_EV in policy_oids:
         is_psd2 = ts_119_495_asn1.id_etsi_psd2_qcStatement in qualified_statement_ids
 
         if is_psd2:
-            return CertificateType.QEVCP_W_PSD2
+            return (CertificateType.QEVCP_W_PSD2_PRE_CERTIFICATE if is_precert
+                    else CertificateType.QEVCP_W_PSD2_FINAL_CERTIFICATE)
         elif is_qualified:
-            return CertificateType.QEVCP_W
+            return CertificateType.QEVCP_W_PRE_CERTIFICATE if is_precert else CertificateType.QEVCP_W_FINAL_CERTIFICATE
         else:
-            return CertificateType.EVCP
+            return CertificateType.EVCP_PRE_CERTIFICATE if is_precert else CertificateType.EVCP_FINAL_CERTIFICATE
     elif serverauth_constants.ID_POLICY_OV in policy_oids:
-        return CertificateType.QNCP_W_OV if is_qualified else CertificateType.OVCP
+        if is_qualified:
+            return (CertificateType.QNCP_W_OV_PRE_CERTIFICATE if is_precert
+                    else CertificateType.QNCP_W_OV_FINAL_CERTIFICATE)
+        else:
+            return CertificateType.OVCP_PRE_CERTIFICATE if is_precert else CertificateType.OVCP_FINAL_CERTIFICATE
     elif serverauth_constants.ID_POLICY_IV in policy_oids:
-        return CertificateType.QNCP_W_IV if is_qualified else CertificateType.IVCP
+        if is_qualified:
+            return (CertificateType.QNCP_W_IV_PRE_CERTIFICATE if is_precert
+                    else CertificateType.QNCP_W_IV_FINAL_CERTIFICATE)
+        else:
+            return CertificateType.IVCP_PRE_CERTIFICATE if is_precert else CertificateType.IVCP_FINAL_CERTIFICATE
     elif serverauth_constants.ID_POLICY_DV in policy_oids:
-        return CertificateType.QNCP_W_DV if is_qualified else CertificateType.DVCP
+        if is_qualified:
+            return (CertificateType.QNCP_W_DV_PRE_CERTIFICATE if is_precert
+                    else CertificateType.QNCP_W_DV_FINAL_CERTIFICATE)
+        else:
+            return CertificateType.DVCP_PRE_CERTIFICATE if is_precert else CertificateType.DVCP_FINAL_CERTIFICATE
     else:
         is_natural_person = any((
             cert.get_subject_attributes_by_type(rfc5280.id_at_givenName),
@@ -42,9 +56,19 @@ def determine_certificate_type(cert: certificate.RFC5280Certificate) -> Certific
         ))
 
         if is_natural_person:
-            return CertificateType.QNCP_W_GEN_NP if is_qualified else CertificateType.NCP_NP
+            if is_qualified:
+                return (CertificateType.QNCP_W_GEN_NP_PRE_CERTIFICATE if is_precert
+                        else CertificateType.QNCP_W_GEN_NP_FINAL_CERTIFICATE)
+            else:
+                return (CertificateType.NCP_NP_PRE_CERTIFICATE if is_precert
+                        else CertificateType.NCP_NP_FINAL_CERTIFICATE)
         else:
-            return CertificateType.QNCP_W_GEN_LP if is_qualified else CertificateType.NCP_LP
+            if is_qualified:
+                return (CertificateType.QNCP_W_GEN_LP_PRE_CERTIFICATE if is_precert
+                        else CertificateType.QNCP_W_GEN_LP_FINAL_CERTIFICATE)
+            else:
+                return (CertificateType.NCP_LP_PRE_CERTIFICATE if is_precert
+                        else CertificateType.NCP_LP_FINAL_CERTIFICATE)
 
 
 def create_decoding_validators() -> List[validation.Validator]:
--- pkilint/etsi/etsi_constants.py
+++ pkilint/etsi/etsi_constants.py
@@ -1,4 +1,7 @@
 import enum
+from enum import auto
+
+from pkilint.cabf.serverauth import serverauth_constants
 
 
 EN_319_412_VERSION = '2023-09'
@@ -6,19 +9,32 @@ EN_319_412_VERSION = '2023-09'
 
 @enum.unique
 class CertificateType(enum.IntEnum):
-    DVCP = 1,
-    IVCP = 2,
-    OVCP = 3,
-    EVCP = 4,
-    NCP_NP = 5,
-    NCP_LP = 6,
-    QEVCP_W = 7,
-    QNCP_W_DV = 8,
-    QNCP_W_IV = 9,
-    QNCP_W_OV = 10,
-    QNCP_W_GEN_NP = 11,
-    QNCP_W_GEN_LP = 12,
-    QEVCP_W_PSD2 = 13,
+    DVCP_PRE_CERTIFICATE = auto()
+    IVCP_PRE_CERTIFICATE = auto()
+    OVCP_PRE_CERTIFICATE = auto()
+    EVCP_PRE_CERTIFICATE = auto()
+    NCP_NP_PRE_CERTIFICATE = auto()
+    NCP_LP_PRE_CERTIFICATE = auto()
+    QEVCP_W_PRE_CERTIFICATE = auto()
+    QNCP_W_DV_PRE_CERTIFICATE = auto()
+    QNCP_W_IV_PRE_CERTIFICATE = auto()
+    QNCP_W_OV_PRE_CERTIFICATE = auto()
+    QNCP_W_GEN_NP_PRE_CERTIFICATE = auto()
+    QNCP_W_GEN_LP_PRE_CERTIFICATE = auto()
+    QEVCP_W_PSD2_PRE_CERTIFICATE = auto()
+    DVCP_FINAL_CERTIFICATE = auto()
+    IVCP_FINAL_CERTIFICATE = auto()
+    OVCP_FINAL_CERTIFICATE = auto()
+    EVCP_FINAL_CERTIFICATE = auto()
+    NCP_NP_FINAL_CERTIFICATE = auto()
+    NCP_LP_FINAL_CERTIFICATE = auto()
+    QEVCP_W_FINAL_CERTIFICATE = auto()
+    QNCP_W_DV_FINAL_CERTIFICATE = auto()
+    QNCP_W_IV_FINAL_CERTIFICATE = auto()
+    QNCP_W_OV_FINAL_CERTIFICATE = auto()
+    QNCP_W_GEN_NP_FINAL_CERTIFICATE = auto()
+    QNCP_W_GEN_LP_FINAL_CERTIFICATE = auto()
+    QEVCP_W_PSD2_FINAL_CERTIFICATE = auto()
 
     def __str__(self):
         return self.name
@@ -34,10 +50,28 @@ class CertificateType(enum.IntEnum):
         return CertificateType[value]
 
 
-CABF_DV_CERTIFICATE_TYPES = {CertificateType.DVCP, CertificateType.QNCP_W_DV}
-CABF_IV_CERTIFICATE_TYPES = {CertificateType.IVCP, CertificateType.QNCP_W_IV}
-CABF_OV_CERTIFICATE_TYPES = {CertificateType.OVCP, CertificateType.QNCP_W_OV}
-CABF_EV_CERTIFICATE_TYPES = {CertificateType.EVCP, CertificateType.QEVCP_W, CertificateType.QEVCP_W_PSD2}
+DVCP_CERTIFICATE_TYPES = {CertificateType.DVCP_PRE_CERTIFICATE, CertificateType.DVCP_FINAL_CERTIFICATE}
+IVCP_CERTIFICATE_TYPES = {CertificateType.IVCP_PRE_CERTIFICATE, CertificateType.IVCP_FINAL_CERTIFICATE}
+OVCP_CERTIFICATE_TYPES = {CertificateType.OVCP_PRE_CERTIFICATE, CertificateType.OVCP_FINAL_CERTIFICATE}
+EVCP_CERTIFICATE_TYPES = {CertificateType.EVCP_PRE_CERTIFICATE, CertificateType.EVCP_FINAL_CERTIFICATE}
+NCP_NP_CERTIFICATE_TYPES = {CertificateType.NCP_NP_PRE_CERTIFICATE, CertificateType.NCP_NP_FINAL_CERTIFICATE}
+NCP_LP_CERTIFICATE_TYPES = {CertificateType.NCP_LP_PRE_CERTIFICATE, CertificateType.NCP_LP_FINAL_CERTIFICATE}
+QEVCP_W_CERTIFICATE_TYPES = {CertificateType.QEVCP_W_PRE_CERTIFICATE, CertificateType.QEVCP_W_FINAL_CERTIFICATE}
+QNCP_W_DV_CERTIFICATE_TYPES = {CertificateType.QNCP_W_DV_PRE_CERTIFICATE, CertificateType.QNCP_W_DV_FINAL_CERTIFICATE}
+QNCP_W_IV_CERTIFICATE_TYPES = {CertificateType.QNCP_W_IV_PRE_CERTIFICATE, CertificateType.QNCP_W_IV_FINAL_CERTIFICATE}
+QNCP_W_OV_CERTIFICATE_TYPES = {CertificateType.QNCP_W_OV_PRE_CERTIFICATE, CertificateType.QNCP_W_OV_FINAL_CERTIFICATE}
+QNCP_W_GEN_NP_CERTIFICATE_TYPES = {CertificateType.QNCP_W_GEN_NP_PRE_CERTIFICATE,
+                                   CertificateType.QNCP_W_GEN_NP_FINAL_CERTIFICATE}
+QNCP_W_GEN_LP_CERTIFICATE_TYPES = {CertificateType.QNCP_W_GEN_LP_PRE_CERTIFICATE,
+                                   CertificateType.QNCP_W_GEN_LP_FINAL_CERTIFICATE}
+QEVCP_W_PSD2_CERTIFICATE_TYPES = {CertificateType.QEVCP_W_PSD2_PRE_CERTIFICATE,
+                                  CertificateType.QEVCP_W_PSD2_FINAL_CERTIFICATE}
+
+
+CABF_DV_CERTIFICATE_TYPES = DVCP_CERTIFICATE_TYPES | QNCP_W_DV_CERTIFICATE_TYPES
+CABF_IV_CERTIFICATE_TYPES = IVCP_CERTIFICATE_TYPES | QNCP_W_IV_CERTIFICATE_TYPES
+CABF_OV_CERTIFICATE_TYPES = OVCP_CERTIFICATE_TYPES | QNCP_W_OV_CERTIFICATE_TYPES
+CABF_EV_CERTIFICATE_TYPES = EVCP_CERTIFICATE_TYPES | QEVCP_W_CERTIFICATE_TYPES | QEVCP_W_PSD2_CERTIFICATE_TYPES
 
 CABF_CERTIFICATE_TYPES = (
         CABF_DV_CERTIFICATE_TYPES |
@@ -46,11 +80,35 @@ CABF_CERTIFICATE_TYPES = (
         CABF_EV_CERTIFICATE_TYPES
 )
 
-NATURAL_PERSON_CERTIFICATE_TYPES = {CertificateType.IVCP, CertificateType.QNCP_W_IV, CertificateType.QNCP_W_GEN_NP}
+NATURAL_PERSON_CERTIFICATE_TYPES = (CABF_IV_CERTIFICATE_TYPES | QNCP_W_GEN_NP_CERTIFICATE_TYPES |
+                                    NCP_NP_CERTIFICATE_TYPES)
 
-LEGAL_PERSON_CERTIFICATE_TYPES = {CertificateType.OVCP, CertificateType.EVCP, CertificateType.QNCP_W_IV,
-                                  CertificateType.QEVCP_W, CertificateType.QEVCP_W_PSD2, CertificateType.QNCP_W_GEN_LP}
+LEGAL_PERSON_CERTIFICATE_TYPES = (CABF_OV_CERTIFICATE_TYPES | CABF_EV_CERTIFICATE_TYPES |
+                                  QNCP_W_GEN_LP_CERTIFICATE_TYPES | NCP_LP_CERTIFICATE_TYPES)
 
-QWAC_TYPES = {CertificateType.QEVCP_W, CertificateType.QNCP_W_DV, CertificateType.QNCP_W_OV, CertificateType.QNCP_W_IV,
-              CertificateType.QNCP_W_GEN_NP, CertificateType.QNCP_W_GEN_LP, CertificateType.QEVCP_W_PSD2
-              }
+QWAC_TYPES = (QEVCP_W_CERTIFICATE_TYPES | QNCP_W_DV_CERTIFICATE_TYPES | QNCP_W_IV_CERTIFICATE_TYPES |
+              QNCP_W_OV_CERTIFICATE_TYPES | QNCP_W_GEN_NP_CERTIFICATE_TYPES | QNCP_W_GEN_LP_CERTIFICATE_TYPES |
+              QEVCP_W_PSD2_CERTIFICATE_TYPES)
+
+
+ETSI_TYPE_TO_CABF_SERVERAUTH_TYPE_MAPPINGS = {
+    CertificateType.DVCP_PRE_CERTIFICATE: serverauth_constants.CertificateType.DV_PRE_CERTIFICATE,
+    CertificateType.IVCP_PRE_CERTIFICATE: serverauth_constants.CertificateType.IV_PRE_CERTIFICATE,
+    CertificateType.OVCP_PRE_CERTIFICATE: serverauth_constants.CertificateType.OV_PRE_CERTIFICATE,
+    CertificateType.EVCP_PRE_CERTIFICATE: serverauth_constants.CertificateType.EV_PRE_CERTIFICATE,
+    CertificateType.QEVCP_W_PRE_CERTIFICATE: serverauth_constants.CertificateType.EV_PRE_CERTIFICATE,
+    CertificateType.QNCP_W_DV_PRE_CERTIFICATE: serverauth_constants.CertificateType.DV_PRE_CERTIFICATE,
+    CertificateType.QNCP_W_IV_PRE_CERTIFICATE: serverauth_constants.CertificateType.IV_PRE_CERTIFICATE,
+    CertificateType.QNCP_W_OV_PRE_CERTIFICATE: serverauth_constants.CertificateType.OV_PRE_CERTIFICATE,
+    CertificateType.QEVCP_W_PSD2_PRE_CERTIFICATE: serverauth_constants.CertificateType.EV_PRE_CERTIFICATE,
+
+    CertificateType.DVCP_FINAL_CERTIFICATE: serverauth_constants.CertificateType.DV_FINAL_CERTIFICATE,
+    CertificateType.IVCP_FINAL_CERTIFICATE: serverauth_constants.CertificateType.IV_FINAL_CERTIFICATE,
+    CertificateType.OVCP_FINAL_CERTIFICATE: serverauth_constants.CertificateType.OV_FINAL_CERTIFICATE,
+    CertificateType.EVCP_FINAL_CERTIFICATE: serverauth_constants.CertificateType.EV_FINAL_CERTIFICATE,
+    CertificateType.QEVCP_W_FINAL_CERTIFICATE: serverauth_constants.CertificateType.EV_FINAL_CERTIFICATE,
+    CertificateType.QNCP_W_DV_FINAL_CERTIFICATE: serverauth_constants.CertificateType.DV_FINAL_CERTIFICATE,
+    CertificateType.QNCP_W_IV_FINAL_CERTIFICATE: serverauth_constants.CertificateType.IV_FINAL_CERTIFICATE,
+    CertificateType.QNCP_W_OV_FINAL_CERTIFICATE: serverauth_constants.CertificateType.OV_FINAL_CERTIFICATE,
+    CertificateType.QEVCP_W_PSD2_FINAL_CERTIFICATE: serverauth_constants.CertificateType.EV_FINAL_CERTIFICATE,
+}
