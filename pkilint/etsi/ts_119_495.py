3bbae4c0d1d6e391340e6a77ebb141eaceb4cbb2
Alex Campbell <campbellalex321@gmail>
2024-02-08 14:42:21 -0700

    removed the stringify - unneeded

--- pkilint/etsi/ts_119_495.py
+++ pkilint/etsi/ts_119_495.py
@@ -40,15 +40,15 @@ class RolesOfPspValidator(validation.Validator):
     def __init__(self):
         super().__init__(validations=[self.VALIDATION_PSP_ROLES_EMPTY, self.VALIDATION_PSP_ROLES_INVALID, self.VALIDATION_PSP_ROLES_MISMATCH,
         self.VALIDATION_PSP_ROLES_UNSPECIFIED, self.VALIDATION_PSP_OIDS_INVALID], pdu_class=ts_119_495_asn1.RolesOfPSP)
-        self._expected_roles = {str(ts_119_495_asn1.id_psd2_role_psp_ai): 'PSP_AI', str(ts_119_495_asn1.id_psd2_role_psp_as): 'PSP_AS',
-        str(ts_119_495_asn1.id_psd2_role_psp_ic): 'PSP_IC', str(ts_119_495_asn1.id_psd2_role_psp_pi): 'PSP_PI', str(ts_119_495_asn1.id_psd2_role_psp_unspecified): 'Unspecified'}
+        self._expected_roles = {ts_119_495_asn1.id_psd2_role_psp_ai: 'PSP_AI', ts_119_495_asn1.id_psd2_role_psp_as: 'PSP_AS',
+        ts_119_495_asn1.id_psd2_role_psp_ic: 'PSP_IC', ts_119_495_asn1.id_psd2_role_psp_pi: 'PSP_PI', ts_119_495_asn1.id_psd2_role_psp_unspecified: 'Unspecified'}
 
     def validate(self, node):
         if not any(node.children):
             raise validation.ValidationFindingEncountered(self.VALIDATION_PSP_ROLES_EMPTY)
         
         for children in node.children.values():
-            psp_oid = str(children.pdu['roleOfPspOid'])
+            psp_oid = children.pdu['roleOfPspOid']
             role_psp = str(children.pdu['roleOfPspName'])
             expected_role = self._expected_roles.get(psp_oid)
 
