914a4e843431134acea5d311b0a02ded501b18bb
Corey Bonnell <corey.bonnell@digicert.com>
2024-04-10 17:12:27 -0400

    Extend graceful decode error handling to all CLI tools (#68)

--- pkilint/bin/lint_cabf_smime_cert.py
+++ pkilint/bin/lint_cabf_smime_cert.py
@@ -116,7 +116,11 @@ def main(cli_args=None) -> int:
 
         return 0
     else:
-        cert = loader.load_certificate(args.file, args.file.name)
+        try:
+            cert = loader.load_certificate(args.file, args.file.name)
+        except ValueError as e:
+            print(f'Failed to load certificate: {e}', file=sys.stderr)
+            return 1
 
         if args.detect:
             v_g = smime.determine_validation_level_and_generation(cert, args.mapping)
