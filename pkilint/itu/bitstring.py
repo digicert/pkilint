92379f424fe2a2cd0ca92f353e1c06c88b410932
Corey Bonnell <corey.bonnell@digicert.com>
2024-11-22 12:13:47 -0500

    Add public key algorithm <-> Key Usage value consistency validator (#139)

--- pkilint/itu/bitstring.py
+++ pkilint/itu/bitstring.py
@@ -1,3 +1,7 @@
 def has_named_bit(node, bit_name):
     bit = node.pdu.namedValues[bit_name]
     return len(node.pdu) > bit and node.pdu[bit] != 0
+
+
+def get_asserted_bit_set(node):
+    return {str(b) for b in node.pdu.namedValues if has_named_bit(node, str(b))}
--- pkilint/pkix/certificate/certificate_key.py
+++ pkilint/pkix/certificate/certificate_key.py
@@ -4,9 +4,11 @@ from cryptography.hazmat.primitives import hashes
 from pyasn1.codec.der.encoder import encode
 from pyasn1.error import PyAsn1Error
 from pyasn1.type import univ
-from pyasn1_alt_modules import rfc5280
+from pyasn1_alt_modules import rfc5280, rfc8410, rfc3279, rfc5480
 
 from pkilint import validation, util, document
+from pkilint.itu import bitstring
+from pkilint.pkix.certificate.certificate_extension import KeyUsageBitName
 from pkilint.pkix.key import verify_signature
 
 
@@ -222,3 +224,178 @@ class AllowedPublicKeyAlgorithmEncodingValidator(validation.Validator):
             raise validation.ValidationFindingEncountered(
                 self._validations[0], f"Prohibited encoding: {encoded_str}"
             )
+
+
+class SpkiKeyUsageConsistencyValidator(validation.Validator):
+    # all bits are allowed except for keyAgreement, see RFC 4055 section 1.2
+    _RSA_ALLOWED_KEY_USAGES = {
+        KeyUsageBitName.DIGITAL_SIGNATURE,
+        KeyUsageBitName.NON_REPUDIATION,
+        KeyUsageBitName.KEY_CERT_SIGN,
+        KeyUsageBitName.CRL_SIGN,
+        KeyUsageBitName.KEY_ENCIPHERMENT,
+        KeyUsageBitName.DATA_ENCIPHERMENT,
+        KeyUsageBitName.DECIPHER_ONLY,
+        KeyUsageBitName.ENCIPHER_ONLY,
+    }
+    VALIDATION_RSA_PROHIBITED_KEY_USAGE_VALUE = validation.ValidationFinding(
+        validation.ValidationFindingSeverity.ERROR,
+        "pkix.key_usage_value_prohibited_for_rsa",
+    )
+
+    # all bits are allowed except for keyEncipherment and dataEncipherment, see RFC 8813 section 3
+    _EC_ALLOWED_KEY_USAGES = {
+        KeyUsageBitName.DIGITAL_SIGNATURE,
+        KeyUsageBitName.NON_REPUDIATION,
+        KeyUsageBitName.KEY_CERT_SIGN,
+        KeyUsageBitName.CRL_SIGN,
+        KeyUsageBitName.KEY_AGREEMENT,
+        KeyUsageBitName.DECIPHER_ONLY,
+        KeyUsageBitName.ENCIPHER_ONLY,
+    }
+    VALIDATION_EC_PROHIBITED_KEY_USAGE_VALUE = validation.ValidationFinding(
+        validation.ValidationFindingSeverity.ERROR,
+        "pkix.key_usage_value_prohibited_for_ec",
+    )
+
+    # see RFC 9295, section 3
+    _X448_AND_X25519_REQUIRED_KEY_USAGES = {
+        KeyUsageBitName.KEY_AGREEMENT,
+    }
+    VALIDATION_EDWARDS_MISSING_REQUIRED_KEY_USAGE_VALUE = validation.ValidationFinding(
+        validation.ValidationFindingSeverity.ERROR,
+        "pkix.key_usage_value_required_but_missing_for_edwards_curve",
+    )
+
+    _X448_AND_X25519_ALLOWED_KEY_USAGES = {
+        KeyUsageBitName.KEY_AGREEMENT,
+        KeyUsageBitName.DECIPHER_ONLY,
+        KeyUsageBitName.ENCIPHER_ONLY,
+    }
+    VALIDATION_EDWARDS_PROHIBITED_KEY_USAGE_VALUE = validation.ValidationFinding(
+        validation.ValidationFindingSeverity.ERROR,
+        "pkix.key_usage_value_prohibited_for_edwards_curve",
+    )
+
+    _SIGNATURE_ALGORITHM_ALLOWED_KEY_USAGES = {
+        KeyUsageBitName.DIGITAL_SIGNATURE,
+        KeyUsageBitName.NON_REPUDIATION,
+        KeyUsageBitName.KEY_CERT_SIGN,
+        KeyUsageBitName.CRL_SIGN,
+    }
+    VALIDATION_SIGNATURE_ALGORITHM_PROHIBITED_KEY_USAGE_VALUE = (
+        validation.ValidationFinding(
+            validation.ValidationFindingSeverity.ERROR,
+            "pkix.key_usage_value_prohibited_for_signature_algorithm",
+        )
+    )
+
+    # _KEM_ALLOWED_KEY_USAGES = {KeyUsageBitName.KEY_ENCIPHERMENT}
+    # VALIDATION_KEM_PROHIBITED_KEY_USAGE_VALUE = validation.ValidationFinding(
+    #    validation.ValidationFindingSeverity.ERROR,
+    #    "pkix.prohibited_key_usage_value_kem",
+    # )
+
+    VALIDATION_UNSUPPORTED_PUBLIC_KEY_ALGORITHM = validation.ValidationFinding(
+        validation.ValidationFindingSeverity.NOTICE,
+        "pkix.public_key_algorithm_unsupported",
+    )
+
+    _KEY_USAGE_VALUE_ALLOWANCES = {
+        rfc3279.rsaEncryption: (
+            (_RSA_ALLOWED_KEY_USAGES, VALIDATION_RSA_PROHIBITED_KEY_USAGE_VALUE),
+            None,
+        ),
+        rfc5480.id_ecPublicKey: (
+            (_EC_ALLOWED_KEY_USAGES, VALIDATION_EC_PROHIBITED_KEY_USAGE_VALUE),
+            None,
+        ),
+        rfc8410.id_X448: (
+            (
+                _X448_AND_X25519_ALLOWED_KEY_USAGES,
+                VALIDATION_EDWARDS_PROHIBITED_KEY_USAGE_VALUE,
+            ),
+            (
+                _X448_AND_X25519_REQUIRED_KEY_USAGES,
+                VALIDATION_EDWARDS_MISSING_REQUIRED_KEY_USAGE_VALUE,
+            ),
+        ),
+        rfc8410.id_X25519: (
+            (
+                _X448_AND_X25519_ALLOWED_KEY_USAGES,
+                VALIDATION_EDWARDS_PROHIBITED_KEY_USAGE_VALUE,
+            ),
+            (
+                _X448_AND_X25519_REQUIRED_KEY_USAGES,
+                VALIDATION_EDWARDS_MISSING_REQUIRED_KEY_USAGE_VALUE,
+            ),
+        ),
+        rfc8410.id_Ed448: (
+            (
+                _SIGNATURE_ALGORITHM_ALLOWED_KEY_USAGES,
+                VALIDATION_SIGNATURE_ALGORITHM_PROHIBITED_KEY_USAGE_VALUE,
+            ),
+            None,
+        ),
+        rfc8410.id_Ed25519: (
+            (
+                _SIGNATURE_ALGORITHM_ALLOWED_KEY_USAGES,
+                VALIDATION_SIGNATURE_ALGORITHM_PROHIBITED_KEY_USAGE_VALUE,
+            ),
+            None,
+        ),
+    }
+
+    def __init__(self):
+        super().__init__(
+            validations=[
+                self.VALIDATION_UNSUPPORTED_PUBLIC_KEY_ALGORITHM,
+                self.VALIDATION_EC_PROHIBITED_KEY_USAGE_VALUE,
+                self.VALIDATION_EDWARDS_PROHIBITED_KEY_USAGE_VALUE,
+                self.VALIDATION_EDWARDS_MISSING_REQUIRED_KEY_USAGE_VALUE,
+                self.VALIDATION_RSA_PROHIBITED_KEY_USAGE_VALUE,
+                self.VALIDATION_SIGNATURE_ALGORITHM_PROHIBITED_KEY_USAGE_VALUE,
+            ],
+            pdu_class=rfc5280.KeyUsage,
+        )
+
+    def validate(self, node):
+        spki_alg_oid = node.navigate(
+            ":certificate.tbsCertificate.subjectPublicKeyInfo.algorithm.algorithm"
+        ).pdu
+
+        allowances = self._KEY_USAGE_VALUE_ALLOWANCES.get(spki_alg_oid)
+
+        if allowances is None:
+            raise validation.ValidationFindingEncountered(
+                self.VALIDATION_UNSUPPORTED_PUBLIC_KEY_ALGORITHM,
+                f"Unsupported public key algorithm: {str(spki_alg_oid)}",
+            )
+
+        allowed_values_and_finding, required_values_and_finding = allowances
+        allowed_values, prohibited_finding = allowed_values_and_finding
+
+        bit_set = bitstring.get_asserted_bit_set(node)
+
+        prohibited_bits = bit_set - allowed_values
+
+        if any(prohibited_bits):
+            prohibited_ku_names = ", ".join(sorted(prohibited_bits))
+
+            raise validation.ValidationFindingEncountered(
+                prohibited_finding,
+                f"Prohibited key usage value(s) present: {prohibited_ku_names}",
+            )
+
+        if required_values_and_finding is not None:
+            required_values, missing_finding = required_values_and_finding
+
+            missing_kus = required_values - bit_set
+
+            if any(missing_kus):
+                missing_ku_names = ", ".join(sorted(missing_kus))
+
+                raise validation.ValidationFindingEncountered(
+                    missing_finding,
+                    f"Required key usage value(s) missing: {missing_ku_names}",
+                )
